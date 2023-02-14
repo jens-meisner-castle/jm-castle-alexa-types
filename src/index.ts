2bb62bb7db873547692fb8c0044a52da69c36c26
Jens Meisner <jnsmsnr@gmail.com>
2023-02-14 10:25:35 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export interface SystemSpec {
+  name?: string;
+  host: string;
+  port: number;
+  certs: {
+    ca?: string;
+    hostCert: string;
+    hostKey: string;
+  };
+  client?: { path: string };
+}
+
+export interface ClientSettings {
+  id: string;
+  ip: string[];
+  user: string;
+}
+
+export interface Configuration {
+  system: SystemSpec;
+  user: {"ask-id": string};
+}
+
+export interface CheckedConfiguration extends Configuration {
+  isValid?: boolean;
+}
+
+export interface SystemStatus {
+  startedAt: number;
+  configuration: {
+    content: CheckedConfiguration;
+    errors: string[] | undefined;
+    valid: CheckedConfiguration;
+  };
+}
